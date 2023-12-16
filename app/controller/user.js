c109afc48e61482bd759a651c5fc15f9c19318c1
nishant.rajput <nishant.rajput@appventurez.com>
2023-12-16 15:15:02 +0530

    check server api done

--- app/controller/user.js
+++ app/controller/user.js
@@ -17,7 +17,11 @@ exports.saveUser = async (req, res) => {
     response.success(req, res, savedUserResponse, httpStatus.CREATED);
   } catch (error) {
     // console.log("Error in saving", error);
-    response.error(req,res,error,httpStatus.INTERNAL_SERVER_ERROR);
+    response.error(req, res, error, httpStatus.INTERNAL_SERVER_ERROR);
   }
 };
+
+exports.checkServer = (req, res) => {
+  return res.json({ message: "Server is working fine." });
+}
 
--- app/router/v1/user/user.js
+++ app/router/v1/user/user.js
@@ -3,6 +3,7 @@ const controller = require('../../../controller/user');
 const {validate} = require("../../../middleware");
 const schema = require("../../../validation/user")
 
+router.get(`/check-server`,controller.checkServer);
 router.post(`/save-user`,validate(schema.userDetail) ,controller.saveUser);
 
 
